5c235a36cafbdfefb5f5f893895515186a9e6b51
Sean Leonard <02musdang@gmail.com>
2023-04-23 13:21:58 -0700

    aut

--- src/auth/authProviderNav.js
+++ src/auth/authProviderNav.js
@@ -7,7 +7,9 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
 
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const redirectUri = 'https://captain-convo-ui.vercel.app/';
+  // const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
