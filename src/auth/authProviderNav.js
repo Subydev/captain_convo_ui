6303ad7ecf78905cfd04ced38e0f17dc750df280
Sean Leonard <02musdang@gmail.com>
2023-04-23 17:56:18 -0700

    redirect

--- src/auth/authProviderNav.js
+++ src/auth/authProviderNav.js
@@ -7,8 +7,8 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
 
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  // const redirectUri = 'https://captain-convo-ui.vercel.app/';
-  const redirectUri = 'http://localhost:3000/';
+  const redirectUri = 'https://captain-convo-ui.vercel.app/';
+  // const redirectUri = 'http://localhost:3000/';
   // const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
 
 
